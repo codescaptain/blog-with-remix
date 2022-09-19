67a81be8c92aacd58d7f221839e3a100adb6070b
codescaptain <codescaptain@gmail.com>
2022-09-19 17:16:32 +0300

    [add] a new post

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,15 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-    </div>
-  );
-}
+    return (
+        <div className="flex justify-center">
+            <div className="prose lg:prose-xl py-10">
+            <h1>Welcome to My Dummy Blog</h1>
+                <div className="flex justify-center">
+                    <Link className="text-right bg-black text-white text-xl border border-gray-500 px-4 py-3 rounded no-underline hover:text-decorition-0 animate-bounce" to="/blog">Go To Blog Page</Link>
+                </div>
+            </div>
+        </div>
+    );
+  }
+  
